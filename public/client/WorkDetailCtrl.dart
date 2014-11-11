d42b2bfdd126efb49720f30407f9b220da1b1cee
Søren Brønsted <sbr@ufds.dk>
2014-11-11 11:01:51 +0100

    Fixed error on new work entry

--- public/client/WorkDetailCtrl.dart
+++ public/client/WorkDetailCtrl.dart
@@ -6,10 +6,10 @@ class WorkDetailCtrl extends BaseDetailCtrl {
 
   List<Future> loadTypes(WorkDetailView view) {
     var parts = Address.instance.getHashUrlElements();
-    List<Future> result = new List();
-    if (parts.length > 3) {
-      view.taskUid = parts[3];
+    if (parts.last == 'new') {
+      view.taskUid = parts[parts.length - 3];
     }
+    List<Future> result = new List();
     Future f2 = Rest.instance.get('/rest/User').then((data)  => view.setUsers(data));
     result.add(f2);
     return result;
